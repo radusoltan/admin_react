47eb66ca9e94f7788304502efb9a7b7c0f8eac35
radusoltan <radusoltan@gmail.com>
2022-02-19 16:03:32 +0100

    multilanguage ready

---
+++ src/i18n.js
@@ -0,0 +1,40 @@
+import i18n from "i18next"
+import { initReactI18next } from "react-i18next"
+import en from './translations/en/admin.json'
+import ro from './translations/ro/admin.json'
+import ru from './translations/ru/admin.json'
+
+const resources = {
+  en: {
+    translation: en
+  },
+  ro: {
+    translation: ro
+  },
+  ru: {
+    translation: ru
+  }
+}
+
+i18n.use(initReactI18next).init({
+  resources,
+  supportedLngs: ["ro", "ru", "en"],
+  fallbackLng: "ro",
+  // detection: {
+  //   order: [
+  //     "cookie",
+  //     "localStorage",
+  //     "htmlTag",
+  //     "sessionStorage",
+  //     "path",
+  //     "subdomain",
+  //   ],
+  //   caches: ["cookie", "localStorage"],
+  // },
+  interpolation: {
+    escapeValue: false, // react already safes from xss
+  },
+  react: { useSuspense: false },
+});
+
+export default i18n
